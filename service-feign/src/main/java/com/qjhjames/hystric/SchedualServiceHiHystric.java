2d33c6fccaca30a8e8de32b6aa4989d825af93f0
qjhjames <qiujunhong1990@126.com>
2017-11-20 16:45:23 +0800

    SOME UPDATE

---
+++ service-feign/src/main/java/com/qjhjames/hystric/SchedualServiceHiHystric.java
@@ -0,0 +1,15 @@
+package com.qjhjames.hystric;
+
+import com.qjhjames.service.SchedualServiceHi;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by user on 2017/11/20.
+ */
+@Component
+public class SchedualServiceHiHystric implements SchedualServiceHi {
+    @Override
+    public String sayHiFromClientOne(String name) {
+        return "sorry "+name;
+    }
+}
--- service-feign/src/main/java/com/qjhjames/service/SchedualServiceHi.java
+++ service-feign/src/main/java/com/qjhjames/service/SchedualServiceHi.java
@@ -1,5 +1,6 @@
 package com.qjhjames.service;
 
+import com.qjhjames.hystric.SchedualServiceHiHystric;
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -8,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 /**
  * Created by user on 2017/11/20.
  */
-@FeignClient(value = "service-hi")
+@FeignClient(value = "service-hi",fallback = SchedualServiceHiHystric.class)
 public interface SchedualServiceHi {
     @RequestMapping(value = "/hi",method = RequestMethod.GET)
     String sayHiFromClientOne(@RequestParam(value = "name") String name);
