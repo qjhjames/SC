ea61731c67c7a160421b4197de2588d4b3e5ebda
裘俊宏 <qiujunhong1990@126.com>
2018-06-21 16:47:14 +0800

    ok

---
+++ myspring/src/main/java/ConfigConstant.java
@@ -0,0 +1,7 @@
+package PACKAGE_NAME;
+
+/**
+ * Created by Administrator on 2018/6/7.
+ */
+public interface ConfigConstant {
+}
---
+++ service-zuul/src/main/java/com/qjhjames/filter/digestredis.java
@@ -0,0 +1,7 @@
+package com.qjhjames.filter;
+
+/**
+ * Created by Administrator on 2018/6/12.
+ */
+public class digestredis {
+}
