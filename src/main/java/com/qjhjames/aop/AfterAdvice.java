6b482efbf9b2e25820a81ff91afa6dcd85db48b8
qiujunhong <qiujunhong1990@126.com>
2018-08-31 21:29:33 +0800

    branchtest

--- src/main/java/com/qjhjames/aop/AfterAdvice.java
+++ src/main/java/com/qjhjames/aop/AfterAdvice.java
@@ -10,6 +10,6 @@ import java.lang.reflect.Method;
 public class AfterAdvice implements AfterReturningAdvice {
     @Override
     public void afterReturning(Object o, Method method, Object[] objects, Object o1) throws Throwable {
-        System.out.println("这是AfterAdvice类的afterReturning方法");//分支test
+        System.out.println("这是AfterAdvice类的afterReturning方法");//分支test hahah
     }
 }
